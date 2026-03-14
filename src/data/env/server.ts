2d4a2f299fe19f08b298930fb88a83c420aeae51
practice12344 <godltxspark@gmail.com>
2026-03-14 16:22:47 +0530

    changes in file

--- src/data/env/server.ts
+++ src/data/env/server.ts
@@ -7,7 +7,7 @@ export const env = createEnv({
     CLERK_SECRET_KEY: z.string().min(1),
     HUME_API_KEY: z.string().min(1),
     HUME_SECRET_KEY: z.string().min(1),
-    GEMINI_API_KEY: z.string().min(1),
+    GOOGLE_GENERATIVE_AI_API_KEY: z.string().min(1),
     DATABASE_URL: z.string().min(1),
   },
 
@@ -18,7 +18,8 @@ export const env = createEnv({
     CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
     HUME_API_KEY: process.env.HUME_API_KEY,
     HUME_SECRET_KEY: process.env.HUME_SECRET_KEY,
-    GEMINI_API_KEY: process.env.GEMINI_API_KEY,
-    DATABASE_URL: process.env.DATABASE_URL, 
+
+    GOOGLE_GENERATIVE_AI_API_KEY: process.env.GOOGLE_GENERATIVE_AI_API_KEY,
+    DATABASE_URL: process.env.DATABASE_URL,
   },
 });
